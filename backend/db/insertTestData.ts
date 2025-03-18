50346fecc28e45c866dbf9f8d5a458dd1f0e7526
Teemu Salonen <teemutapani.salonen@gmail.com>
2025-03-18 20:02:31 +0200

    Asiakasraportti viime vuosi API toteutus

--- backend/db/insertTestData.ts
+++ backend/db/insertTestData.ts
@@ -28,7 +28,7 @@ const kayttajat = [
 		nimi: 'Divarin 2 omistaja',
 		osoite: 'Asiakastie 2',
 		puhelin: '0407654322',
-		email: 'divari2@rmail.com',
+		email: 'divari2@email.com',
 		salasana: await hashSalasana('salasana'),
 		rooli: 'divariAdmin',
 	},
@@ -149,6 +149,16 @@ const teosInstanssit = [
 	{ hinta: 10.0, kunto: 'kohtalainen', sisaanostohinta: 6.0, teosId: '03d2c3b6-f2c4-41c4-a105-9f2bb01dbacd', divariId: 2 },
 	{ hinta: 15.0, kunto: 'heikko', sisaanostohinta: 7.0, teosId: '8a9a88c7-3487-47d5-a73d-1bfcfbaa17ef', divariId: 2 },
 	{ hinta: 35.0, kunto: 'erinomainen', sisaanostohinta: 17.0, teosId: '5c68c167-3770-456e-9385-6d58abdce0d5', divariId: 4 },
+	{ hinta: 10.0, kunto: 'kohtalainen', sisaanostohinta: 5.0, teosId: '03d2c3b6-f2c4-41c4-a105-9f2bb01dbacd', divariId: 2, tilausId: 1 },
+	{ hinta: 10.0, kunto: 'kohtalainen', sisaanostohinta: 6.0, teosId: '03d2c3b6-f2c4-41c4-a105-9f2bb01dbacd', divariId: 2, tilausId: 1 },
+	{ hinta: 15.0, kunto: 'heikko', sisaanostohinta: 7.0, teosId: '8a9a88c7-3487-47d5-a73d-1bfcfbaa17ef', divariId: 2, tilausId: 2 },
+	{ hinta: 35.0, kunto: 'erinomainen', sisaanostohinta: 17.0, teosId: '5c68c167-3770-456e-9385-6d58abdce0d5', divariId: 4, tilausId: 3 },
+];
+
+const tilaukset = [
+	{ tila: 'valmis', tilauspvm: '2025-03-01', postikulut: 5.0, kokonaishinta: 50.0, kayttajaId: 5 },
+	{ tila: 'valmis', tilauspvm: '2024-03-18', postikulut: 10.0, kokonaishinta: 100.0, kayttajaId: 5 },
+	{ tila: 'valmis', tilauspvm: '2024-03-17', postikulut: 15.0, kokonaishinta: 10000.0, kayttajaId: 5 },
 ];
 
 // Lisää testidata tietokantaan
@@ -164,6 +174,7 @@ export const lisaaTestidata = async () => {
 		await db.transaction(async (trx) => {
 			await trx(`${keskusdivari}.Teos`).insert(teokset);
 			await trx(`${keskusdivari}.Divari_Admin`).insert(divariAdminit);
+			await trx(`${keskusdivari}.Tilaus`).insert(tilaukset);
 			await trx(`${keskusdivari}.TeosInstanssi`).insert(teosInstanssit);
 		});
 		console.log('Testidata lisätty onnistuneesti');
